234748ea9e32e453ebddc05e67964ecb83d66658
juliano988 <julio-pinheiroo@hotmail.com>
2020-12-23 18:02:25 -0300

    Versão funcional finalizada

--- criarAlimentosColl.js
+++ criarAlimentosColl.js
@@ -1,4 +1,5 @@
-var cron = require('node-cron');
+const cron = require('node-cron');
+const importarDbOFF = require('./importarDbOFF.js');
 
 //Constantes
 const numDeExecuções = 10;
@@ -31,56 +32,84 @@ db.once('open', function () {
 
     const Alimentos = mongoose.model('alimentos', alimentosSchema);
 
-    //Processo é reiniciado às 1h
-    // cron.schedule('* 1 * * *', () => {
-    //     numDaExecução = 1;
-    // });
+    // É iniciado às 0h o processo de importação da base de ddos do OFF para a base de dados da API.
+    cron.schedule('27 17 * * *', () => {
+        console.clear();
 
-    //Processo é executado a cada 5min
-    let numDaExecução = 1;
+        console.log('Importando dados do OFF para o bando de dados da API.');
+        importarDbOFF();
+    });
+
+    let numDaExecução = Infinity;
+
+    // É iniciado às 1h o processo de formatação da base de dados da API
+    cron.schedule('59 17 * * *', () => {
+        console.clear();
+
+        numDaExecução = 0;
+        console.log(new Date(), ' - Processo de formatação dos dados da API iniciado.');
+        Alimentos.deleteMany({ product_name: /^(?!.)/ }, function (err, data) {
+            if (err) { return console.log(err) };
+            let excluidos1 = data.n;
+            Alimentos.deleteMany({ product_name: { $exists: false } }, function (err, data) {
+                if (err) { return console.log(err) };
+                console.log('Foram excluídos ' + (excluidos1 + data.n) + ' alimentos sem nome.');
+            });
+        });
+    });
+
+    //Processo de formatação da base de dados da API é executado de acordo com a periodicidade definida na constante "intervaloEntreExecuçõesMin"
     cron.schedule('*/' + intervaloEntreExecuçõesMin + ' * * * *', () => {
         console.clear();
-        if (numDaExecução <= numDeExecuções) {
+
+        if (++numDaExecução <= numDeExecuções) {
             console.log(new Date() + ' - Execução ' + numDaExecução + '/' + numDeExecuções);
 
-            Alimentos.find({}).select('code product_name quantity categories packaging brands images barcode status imported_t url image_url').skip((numDaExecução++ - 1) * docsExecutadosporVez).limit(docsExecutadosporVez).exec(function (err, data) {
+            Alimentos.find({}).select('code product_name quantity categories packaging brands images barcode status imported_t url image_url').skip((numDaExecução - 1) * docsExecutadosporVez).limit(docsExecutadosporVez).exec(function (err, data) {
                 if (err) { return console.log(err) };
                 const alimentosSelecionados = data;
                 console.log('Documentos encontrados: ' + alimentosSelecionados.length);
+
                 for (const i in alimentosSelecionados) {
-                    let imageLink;
-                    // Estrutura try catch necessária nos casos onde o alimento não possui imagem.
-                    try {
-                        //if necessário no caso do codigo ser executado mais de uma vez consecutiva.
-                        if (alimentosSelecionados[Number(i)].image_url) {
-                            imageLink = alimentosSelecionados[Number(i)].image_url
-                        } else {
-                            imageLink = "https://static.openfoodfacts.org/images/products/" + alimentosSelecionados[Number(i)].code.substring(0, 3) + '/' + alimentosSelecionados[Number(i)].code.substring(3, 6) + '/' + alimentosSelecionados[Number(i)].code.substring(6, 9) + '/' + alimentosSelecionados[Number(i)].code.substring(9) + '/' + Object.getOwnPropertyNames(alimentosSelecionados[Number(i)].get('images')).find(function (val) { return (/front_/).test(val) }) + '.' + alimentosSelecionados[Number(i)].get('images')[Object.getOwnPropertyNames(alimentosSelecionados[Number(i)].get('images')).find(function (val) { return (/front_/).test(val) })].rev + '.200.jpg';
+
+                    if (alimentosSelecionados[Number(i)].imported_t) {
+                        console.log('Alimento já formatado: ' + alimentosSelecionados[Number(i)].code, alimentosSelecionados[Number(i)].product_name);
+
+                    } else {
+                        let imageLink;
+                        // Estrutura try catch necessária nos casos onde o alimento não possui imagem.
+                        try {
+                            //if necessário no caso do codigo ser executado mais de uma vez consecutiva.
+                            if (alimentosSelecionados[Number(i)].image_url) {
+                                imageLink = alimentosSelecionados[Number(i)].image_url
+                            } else {
+                                imageLink = "https://static.openfoodfacts.org/images/products/" + alimentosSelecionados[Number(i)].code.substring(0, 3) + '/' + alimentosSelecionados[Number(i)].code.substring(3, 6) + '/' + alimentosSelecionados[Number(i)].code.substring(6, 9) + '/' + alimentosSelecionados[Number(i)].code.substring(9) + '/' + Object.getOwnPropertyNames(alimentosSelecionados[Number(i)].get('images')).find(function (val) { return (/front_/).test(val) }) + '.' + alimentosSelecionados[Number(i)].get('images')[Object.getOwnPropertyNames(alimentosSelecionados[Number(i)].get('images')).find(function (val) { return (/front_/).test(val) })].rev + '.200.jpg';
+                            }
+                        } catch (error) {
+                            imageLink = '/img_not_found.png';
                         }
-                    } catch (error) {
-                        imageLink = '/img_not_found.png';
+                        Alimentos.replaceOne({ code: alimentosSelecionados[Number(i)].code },
+                            {
+                                code: alimentosSelecionados[Number(i)].code,
+                                barcode: Number(alimentosSelecionados[Number(i)].code).toString(10).length === 13 ? alimentosSelecionados[Number(i)].code + "(EAN / EAN-13)" : alimentosSelecionados[Number(i)].code + "(EAN / EAN-13) " + alimentosSelecionados[Number(i)].code.substring(1) + "(UPC / UPC-A)",
+                                status: alimentosSelecionados[Number(i)].status || "published",
+                                imported_t: alimentosSelecionados[Number(i)].imported_t || new Date(),
+                                url: "https://world.openfoodfacts.org/product/" + alimentosSelecionados[Number(i)].code,
+                                product_name: alimentosSelecionados[Number(i)].product_name,
+                                quantity: alimentosSelecionados[Number(i)].quantity,
+                                categories: alimentosSelecionados[Number(i)].categories,
+                                packaging: alimentosSelecionados[Number(i)].packaging,
+                                brands: alimentosSelecionados[Number(i)].brands,
+                                image_url: imageLink
+                            }, function (err, data) {
+                                if (err) { return console.log(err) };
+                                console.log('Alimento atualizado: ' + alimentosSelecionados[Number(i)].code, alimentosSelecionados[Number(i)].product_name);
+                            });
                     }
-                    Alimentos.replaceOne({ code: alimentosSelecionados[Number(i)].code },
-                        {
-                            code: alimentosSelecionados[Number(i)].code,
-                            barcode: Number(alimentosSelecionados[Number(i)].code).toString(10).length === 13 ? alimentosSelecionados[Number(i)].code + "(EAN / EAN-13)" : alimentosSelecionados[Number(i)].code + "(EAN / EAN-13) " + alimentosSelecionados[Number(i)].code.substring(1) + "(UPC / UPC-A)",
-                            status: alimentosSelecionados[Number(i)].status || "published",
-                            imported_t: alimentosSelecionados[Number(i)].imported_t || new Date(),
-                            url: "https://world.openfoodfacts.org/product/" + alimentosSelecionados[Number(i)].code,
-                            product_name: alimentosSelecionados[Number(i)].product_name,
-                            quantity: alimentosSelecionados[Number(i)].quantity,
-                            categories: alimentosSelecionados[Number(i)].categories,
-                            packaging: alimentosSelecionados[Number(i)].packaging,
-                            brands: alimentosSelecionados[Number(i)].brands,
-                            image_url: imageLink
-                        }, function (err, data) {
-                            if (err) { return console.log(err) };
-                            console.log('Alimento atualizado: ' + alimentosSelecionados[Number(i)].code, alimentosSelecionados[Number(i)].product_name);
-                        });
                 };
             });
-        } else {
-            console.log(new Date() + ' - Número de execuções necessárias foi alcançado');
+        } else if (numDaExecução !== Infinity) {
+            console.log(new Date() + ' - Número de execuções foi alcançado');
         }
     });
 });
--- importarDbOFF.js
+++ importarDbOFF.js
@@ -10,35 +10,38 @@ const collection = '--collection alimentos';
 const type = '--type json';
 const file = '--file ' + __dirname + '\\arquivosOFF\\';
 
-let inportFlag = 0;
-fetch('https://static.openfoodfacts.org/data/delta/index.txt')
-    .then(function (response) {
-        return response.text(); // or .text() or .blob() ...
-    })
-    .then(function (text) {
-        // text is the response body
-        const nomeArquivos = text.trim().split(/\s/);
-        console.log('Arquivos para download: ' + nomeArquivos.length);
+module.exports = function importarDbOFF() {
+    fetch('https://static.openfoodfacts.org/data/delta/index.txt')
+        .then(function (response) {
+            return response.text(); // or .text() or .blob() ...
+        })
+        .then(function (text) {
+            // text is the response body
+            //Slice inserido apenas para fins de teste para evitar ocupar o limite de espaço maxímo no bando de dados.
+            const nomeArquivos = text.trim().split(/\s/)//.slice(0,10);
+            console.log('Arquivos para download: ' + nomeArquivos.length);
 
-        (async () => {
-            for (let i in nomeArquivos) {
-                const iNum = Number(i);
-                await download('https://static.openfoodfacts.org/data/delta/' + nomeArquivos[iNum], 'arquivosOFF');
-                console.log('Arquivo baixado ' + (iNum + 1).toString(10).padStart(2, '0') + '/' + nomeArquivos.length + ': ' + nomeArquivos[iNum])
-                gunzip('arquivosOFF/' + nomeArquivos[iNum], 'arquivosOFF/' + nomeArquivos[iNum].replace('.gz', ''), function () {
-                    console.log('Arquivo descompactado ' + (iNum + 1).toString(10).padStart(2, '0') + '/' + nomeArquivos.length + ': ' + nomeArquivos[iNum]);
-                });
-                execFile('mongoimport', [uri, collection, type, file + nomeArquivos[iNum].replace('.gz', '')], { shell: true }, function () {
-                    console.log('Arquivo importado ' + (iNum + 1).toString(10).padStart(2, '0') + '/' + nomeArquivos.length + ': ' + nomeArquivos[iNum].replace('.gz', ''));
-                    if (++inportFlag >= nomeArquivos.length) {
-                        del(['arquivosOFF']);
-                        console.log('Processo de importação concluído!');
-                    }
-                });
-            }
-        })();
-    })
-    .catch(function (e) {
-        // error in e.message
-        console.log(e.message);
-    });
+            (async () => {
+                let inportFlag = 0;
+                for (let i in nomeArquivos) {
+                    const iNum = Number(i);
+                    await download('https://static.openfoodfacts.org/data/delta/' + nomeArquivos[iNum], 'arquivosOFF');
+                    console.log('Arquivo baixado ' + (iNum + 1).toString(10).padStart(2, '0') + '/' + nomeArquivos.length + ': ' + nomeArquivos[iNum])
+                    gunzip('arquivosOFF/' + nomeArquivos[iNum], 'arquivosOFF/' + nomeArquivos[iNum].replace('.gz', ''), function () {
+                        console.log('Arquivo descompactado ' + (iNum + 1).toString(10).padStart(2, '0') + '/' + nomeArquivos.length + ': ' + nomeArquivos[iNum]);
+                    });
+                    execFile('mongoimport', [uri, collection, type, file + nomeArquivos[iNum].replace('.gz', '')], { shell: true }, function () {
+                        console.log('Arquivo importado ' + (iNum + 1).toString(10).padStart(2, '0') + '/' + nomeArquivos.length + ': ' + nomeArquivos[iNum].replace('.gz', ''));
+                        if (++inportFlag >= nomeArquivos.length) {
+                            del(['arquivosOFF']);
+                            console.log('Processo de importação concluído!');
+                        }
+                    });
+                }
+            })();
+        })
+        .catch(function (e) {
+            // error in e.message
+            console.log(e.message);
+        });
+}
